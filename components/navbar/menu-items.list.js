f51911cb7cb458b44d6e5ea5f93d78102ab9cce4
Ruben Vitt <dev@rubeen.dev>
2020-07-23 06:40:51 +0200

    🚧(general): some improvements

---
+++ components/navbar/menu-items.list.js
@@ -0,0 +1,6 @@
+export const MenuList = (t) => [
+    {title: t('nav.main_page'), url: '/'},
+    {title: t('nav.cv'), url: '/cv'},
+    {title: t('nav.portfolio'), url: '/portfolio'},
+    {title: t('nav.contact'), url: '/contact'},
+]
--- components/navbar/navbar.component.jsx
+++ components/navbar/navbar.component.jsx
@@ -1,14 +1,20 @@
 import React, {useState} from "react";
-import {MobileMenu} from "../mobile/menu/mobile-menu.component";
+import {MobileMenu} from "./mobile/menu/mobile-menu.component";
 import {useTranslation} from "../../config/i18next.config";
+import {MobileMenuButton} from "./mobile/menu-button/mobile-menu-button";
 
 export const NavBar = () => {
     const [profileMenuOpen, setProfileMenuOpen] = useState(false);
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
     const {t} = useTranslation('common');
 
     const toggleProfileMenu = () => {
         setProfileMenuOpen(!profileMenuOpen);
+    }
+
+    const toggleMobileMenu = () => {
+        setMobileMenuOpen(!mobileMenuOpen);
     }
 
     return <nav className="bg-white border-b border-gray-200">
@@ -63,39 +69,12 @@ export const NavBar = () => {
                                      alt=""/>
                             </button>
                         </div>
-                        <div
-                            className={"origin-top-right " + (!profileMenuOpen ? 'hidden' : '') + " absolute right-0 mt-2 w-48 rounded-md shadow-lg"}>
-                            <div className="py-1 rounded-md bg-white shadow-xs">
-                                <a href="#"
-                                   className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">Your
-                                    Profile</a>
-                                <a href="#"
-                                   className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">Settings</a>
-                                <a href="#"
-                                   className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">Sign
-                                    out</a>
-                            </div>
-                        </div>
                     </div>
                 </div>
-                <div className="-mr-2 flex items-center sm:hidden">
-                    <button
-                        className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
-                        <svg className="block h-6 w-6" stroke="currentColor" fill="none"
-                             viewBox="0 0 24 24">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                  d="M4 6h16M4 12h16M4 18h16"/>
-                        </svg>
-                        <svg className="hidden h-6 w-6" stroke="currentColor" fill="none"
-                             viewBox="0 0 24 24">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                  d="M6 18L18 6M6 6l12 12"/>
-                        </svg>
-                    </button>
-                </div>
+                <MobileMenuButton onClick={toggleMobileMenu}/>
             </div>
         </div>
 
-        <MobileMenu />
+        <MobileMenu isOpen={mobileMenuOpen}/>
     </nav>
 }
