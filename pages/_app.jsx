ea7585c941d84be17de51c984d5b320f2a260b79
Ruben Vitt <dev@rubeen.dev>
2020-07-26 13:06:37 +0200

    ✨(social-buttons): add social buttons to navbar

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,7 @@
-import '../styles/index.css'
+import '../styles/index.scss'
 import NextI18Next from "../config/i18next.config";
 import React from "react";
+import '../components/navbar/style.scss'
 
 function MyApp({Component, pageProps}) {
     return <Component {...pageProps} />
