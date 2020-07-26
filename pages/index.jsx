42f431b814a6c32718a61a0ec2b83973142fc22b
Ruben Vitt <dev@rubeen.dev>
2020-07-26 09:46:01 +0200

    chore(general): some additions

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import {PageHead} from "../components/page-head/page-head.component";
 import {PageContainer} from "../components/page-container/page-container.component";
 
-export default function Home() {
+export default () => {
     return (
         <div>
             <PageHead />
