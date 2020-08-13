4f99fff3aa697e172611169f6d130ace1214296a
Ruben Vitt <dev@rubeen.dev>
2020-08-13 18:13:36 +0200

    ✨(general): more additions

--- data/icons/animation-data/icons.ts
+++ data/icons/animation-data/icons.ts
@@ -1,8 +1,13 @@
 import back from './back.json';
 import wandAndStars from './wand-and-stars.json';
-
+import pinWithEclipseUnder from './pin-with-eclipse-under.json';
+import lightOn from './light-on.json';
+import hourGlass from './hourglass.json';
 
 export const ShapeAnimation = {
     BackAnimation: back,
     WandAndStartAnimation: wandAndStars,
+    PinWithEclipseUnder: pinWithEclipseUnder,
+    LightOn: lightOn,
+    Hourglass: hourGlass,
 };
