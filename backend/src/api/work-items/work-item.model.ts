b26ef9fc223778104e190cf1640cb5c1f72268c7
Ruben Vitt <dev@rubeen.dev>
2020-09-16 21:56:03 +0200

    🔥(backend): remove backend code from this project, rm some dependencies

--- backend/src/api/work-items/work-item.model.ts
+++
@@ -1,3 +0,0 @@
-export class WorkItemModel {
-    id: string;
-}
--- backend/src/schemas/dto/insert-update.work-item.dto.ts
+++
@@ -1,7 +0,0 @@
-import {FindWorkItemDto} from "./find.work-item.dto";
-import {CreateWorkItemDto} from "./create.work-item.dto";
-
-export class InsertUpdateWorkItemDto {
-    readonly filter: FindWorkItemDto;
-    readonly update: CreateWorkItemDto;
-}
